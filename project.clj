e9918ff6352612f37eb0e044d6e3d6908bdffdd1
Mike Fikes <mike@fikesfarm.com>
2020-06-15 13:59:41 -0400

    Fix cljs dep in project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.764" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.597" :scope "provided"]
                  [com.github.rickyclarkson/jmdns "3.4.2-r353-1"]])
