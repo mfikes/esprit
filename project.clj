cf2ef975607b5ab116ff5ab730c194cc0d433874
Mike Fikes <mike@fikesfarm.com>
2020-08-22 09:12:21 -0400

    0.10.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject esprit "0.9.0"
+(defproject esprit "0.10.0"
   :description "ClojureScript on the ESP32 using Espruino"
   :url "https://github.com/mfikes/esprit"
   :source-paths ["src" "resources"]
